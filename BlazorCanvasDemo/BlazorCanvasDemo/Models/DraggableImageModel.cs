9e1789411d6992f025817b793509a9a2283e8967
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-09-04 12:47:52 +0200

    Make draggable image its own component

---
+++ BlazorCanvasDemo/BlazorCanvasDemo/Models/DraggableImageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorCanvasDemo.Models;
+
+public class DraggableImageModel
+{
+    public string Source { get; set; } = "";
+    public double Height { get; set; }
+    public double Width { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+}
